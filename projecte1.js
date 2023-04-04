910414ab1a4e5d2f1a5b7ef3cecf5a6f57cd9271
1639080 <91468276+1639080@users.noreply.github.com>
2023-04-04 10:28:16 +0200

    Add files via upload
    he modificat els atributs que calia encastar/referencia. plus classificat les consultes

--- projecte1.js
+++ projecte1.js
@@ -2,22 +2,12 @@ use ("tendacomics")
 
 db.createCollection("editorial")
 db.editorial.insertOne([
-    "id": ObjectId,
+    "id": ObjectId, 
     "NomEditorial": "str",
     "responsable": "str",
     "adreça": "str",
     "país": "str"
 ])
-
-//a lo millor un atribut de les seves col·leccions 
-//hi ha una consulta que implica editorial-publicacio: 2. Valor màxim, mínim i mitjà del preus de les publicacions de l’editorial Juniper Books
-//però la relacio de editorial-publicacio ve més de editorial-col·leccio-publicació
-//llavors podriem ficar en editorial les colleccions o al reves, en les colleccions ficar una d'editorials
-//només s'hauria de guardar id i nom de l'editorial
-
-//hi ha una altre consulta: 5. Per cada editorial, mostrar el recompte de col·leccions finalitzades i no finalitzades.
-// implica relacio editorial-col·leccio i veure l'atribut tancada.
-//tmb consulta 6 relacional editorial-collection. 6. Mostrar les 2 col·leccions ja finalitzades amb més publicacions. mostrar editorial i nom
 
 db.createCollection("colleccio")
 db.colleccio.insertOne([
@@ -29,12 +19,8 @@ db.colleccio.insertOne([
     "any_inici": int,
     "any_fi": int,
     "tancada": Boolean //true o false
-    //"editorials": [{"id":ObjectId, "nom": "str"},{}]
+    "editorials": [{"id": IDeditorial, "nom": "str"},{}] //guardar id i nom editorials
     ])
-    
-    
-//al igual que abans, caldria un atribut amb editorials que sigui una llista de les editorials id i nom: consulta 2,5 i 6
-//4. Numero de col·leccions per gènere. Mostra gènere i número total.
 
 db.createCollection("publicacio")
 db.publicacio.insertOne([
@@ -45,30 +31,11 @@ db.publicacio.insertOne([
     "autor": "str",
     "preu": int, 
     "num_pagines": int,
+    "colleccions": ["idcolleccions"],  //referenciar colleccions per id
+    "personatges": [ {"_id": ObjectId, "nom": "str","tipus": "str","ISBN": int}, {} ], //ENCASTAR PERSONATGES A PUBLI
+    "artistes": ["nom_artistic"] //referenciar artistes pel nombre artistic
     ])
 
-//1. Les 5 publicacions amb major preu. Mostrar només el títol i preu.
-//9. Modificar el preu de les publicacions de primera edició i incrementar-lo un 25%.
-
-//2. Valor màxim, mínim i mitjà del preus de les publicacions de l’editorial Juniper Books
-//6. Mostrar les 2 col·leccions ja finalitzades amb més publicacions. Mostrar editorial i nom col·lecció.
-// consulta 2 i 6 implica publi-colleccio-editorial. a la collecio tindriem les editorials amb id i nom
-
-// caldria una relacio amb artistes i personatges:fer dos atributs de artistes:[] i personatges: []
-//3. Artistes (nom artístic) que participen en més de 5 publicacions com a dibuixant.
-//10. Mostrar ISBN i títol de les publicacions conjuntament amb tota la seva informació dels personatges.
-
-db.createCollection("personatge")
-db.personatge.insertOne([
-    "_id": ObjectId,
-    "nom": "str",
-    "tipus": "str",
-    "ISBN": int
-    ])
-//8. Mostrar les publicacions amb tots els personatges de tipus “heroe”.
-//10. Mostrar ISBN i títol de les publicacions conjuntament amb tota la seva
-//nformació dels personatges.
-    
     
 db.createCollection("artista")
 db.artista.insertOne([
@@ -80,6 +47,52 @@ db.artista.insertOne([
     "pais": "str",
     "tipus": "str" //guionista o dibuixant
     ])
+
+//Colleccio personatges no cal crear perque embedded en publicacio
+//db.createCollection("personatge")
+//db.personatge.insertOne([
+//    "_id": ObjectId,
+//    "nom": "str",
+//    "tipus": "str",
+//    "ISBN": int
+//    ])
+
+//CONSULTES SOLES:
+
+  //Només colleccio: 
+      //4-Numero de col·leccions per gènere. Mostra gènere i número total.
+  
+  //Només publi:
+      //1-Les 5 publicacions amb major preu. Mostrar només el títol i preu.
+      //9-Modificar el preu de les publicacions de primera edició i incrementar-lo un 25%.
+  
+  //Només artista:
+      //7-Mostrar el país d’origen de l’artista o artistes que han fet més guions.
+
+
+
+//CONSULTES RELACIONADES:
     
-//3. Artistes (nom artístic) que participen en més de 5 publicacions com a dibuixant.
-//7. Mostrar el país d’origen de l’artista o artistes que han fet més guions.
+  //EDITORIAL-PUBLICACIO-COLLECCIO:
+
+      //2-Valor màxim, mínim i mitjà del preus de les publicacions de l’editorial Juniper Books  
+      //5-Per cada editorial, mostrar el recompte de col·leccions finalitzades i no finalitzades (editorial-col·leccio)
+      //6-Mostrar les 2 col·leccions ja finalitzades amb més publicacions. mostrar editorial i nom 
+      // SOLUCIO 2,5,6: 
+          //En les colleccions ficar editorials. Només s'hauria de guardar id i nom de l'editorial.
+          //En publicacions ficar colleccions referencial 
+
+
+  //PUBLICACIO-PERSONATGES:
+  
+      //8-Mostrar les publicacions amb tots els personatges de tipus “heroe”.
+      //10-Mostrar ISBN i títol de les publicacions conjuntament amb tota la seva informació dels personatges.
+      //SOLUCIO 8,10: 
+          //Encastar tota la info dels personatges a publi (eliminar colleccio personatges) 
+
+
+  //PUBLICACIO-ARTISTA: 
+  
+      //3-Artistes (nom artístic) que participen en més de 5 publicacions com a dibuixant.
+      //SOLUCIO: 
+          //guardar nom artistic dels artistes en publi
