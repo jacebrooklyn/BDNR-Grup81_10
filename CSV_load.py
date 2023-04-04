516bf7b1af2b36b39de35b2da4aaf51547430f89
ANDREUMIR <91468402+NIU1637558@users.noreply.github.com>
2023-04-04 11:22:00 +0200

    Update CSV_load.py

--- CSV_load.py
+++ CSV_load.py
@@ -20,11 +20,11 @@ personatges = pd.read_excel("Dades.xlsx", sheet_name='Personatges')
 artists = pd.read_excel("Dades.xlsx", sheet_name='Artistes')
 
 # 3.1 Carreguem les dades de les editorials
+# db.editorial.drop() # eliminem la colecció per si ja hi ha dades
 if db.editorial.count_documents({})==0: # si no hi ha dades a la colecció editorial, les carreguem
     print('\n1. Carregant dades de les editorials...')
     editorial_dict=col_pub[['NomEditorial','resposable','adreca','pais']].to_dict('records') # utilizem format records perque cada fila sigue dict
     db.editorial.insert_many(editorial_dict) # carreguem les dades
-
     # Comprovem que s'han afegit correctament
     if db.editorial.count_documents({})!=26: print("[1] >> Error: No s'han carregat totes les editorials"); sys.exit()
     else: print("[1] >> Dades carregades correctament")
@@ -32,11 +32,16 @@ if db.editorial.count_documents({})==0: # si no hi ha dades a la colecció edito
 else: print('[1] >> Dades ja carregades anteriorment')
 
 # 3.2 Carreguem les dades de les coleccions
+# db.colleccio.drop() # eliminem la colecció per si ja hi ha dades
 if db.colleccio.count_documents({})==0: # si no hi ha dades a la colecció colleccio, les carreguem
     print('\n2. Carregant dades de les colleccions...')
-    colleccio_dict=col_pub[['NomEditorial','NomColleccio','total_exemplars','genere','any_inici','any_fi','idioma','tancada']].to_dict('records') # utilizem format records perque cada fila sigue dict
+    colleccio_dict=col_pub[['NomColleccio','NomEditorial','total_exemplars','genere','any_inici','any_fi','idioma','tancada']].to_dict('records') # utilizem format records perque cada fila sigue dict
+    #afegim els generes com a llistes i tancada a bool
+    gen=col_pub['genere'].apply(lambda x: x.strip('[]').split(','))
+    for i,e in enumerate(colleccio_dict):
+        e['genere'] = gen[i] # inserim la llista de generes
+        e['tancada'] = bool(e['tancada'])
     db.colleccio.insert_many(colleccio_dict) # carreguem les dades
-
     # Comprovem que s'han afegit correctament
     if db.colleccio.count_documents({})!=26: print("[2] >> Error: No s'han carregat totes les col·leccions"); sys.exit()
     else: print("[2] >> Dades carregades correctament")
@@ -44,9 +49,10 @@ if db.colleccio.count_documents({})==0: # si no hi ha dades a la colecció colle
 else: print('[2] >> Dades ja carregades anteriorment')
 
 # 3.3 Carreguem les dades de les publicacions
+# db.publicacio.drop() # eliminem la colecció per si ja hi ha dades
 if db.publicacio.count_documents({})==0: # si no hi ha dades a la colecció publicacio, les carreguem
     print('\n3. Carregant dades de les publicacions...')
-    publicacio_dict=col_pub[['ISBN','NomEditorial','NomColleccio','titol','stock','autor','preu','num_pagines']].to_dict('records') # utilizem format records perque cada fila sigue dict
+    publicacio_dict=col_pub[['ISBN','NomColleccio','titol','stock','autor','preu','num_pagines']].to_dict('records') # utilizem format records perque cada fila sigue dict
     #afegim els guionistes i dibuixants com a llistes
     g = col_pub['guionistes'].apply(lambda x: x.strip('[]').split(','))
     d = col_pub['dibuixants'].apply(lambda x: x.strip('[]').split(','))
@@ -62,11 +68,11 @@ if db.publicacio.count_documents({})==0: # si no hi ha dades a la colecció publ
 else: print('[3] >> Dades ja carregades anteriorment')
 
 # 3.4 Carreguem les dades dels personatges
+# db.personatge.drop() # eliminem la colecció per si ja hi ha dades
 if db.personatge.count_documents({})==0: # si no hi ha dades a la colecció personatge, les carreguem
     print('\n4. Carregant dades dels personatges...')
     personatge_dict=personatges[['nom','tipus']].to_dict('records') # utilizem format records perque cada fila sigue dict
     db.personatge.insert_many(personatge_dict) # carreguem les dades
-
     # Comprovem que s'han afegit correctament
     if db.personatge.count_documents({})!=77: print("[4] >> Error: No s'han carregat tots els personatges"); sys.exit()
     else: print("[4] >> Dades carregades correctament")
@@ -74,6 +80,7 @@ if db.personatge.count_documents({})==0: # si no hi ha dades a la colecció pers
 else: print('[4] >> Dades ja carregades anteriorment')
 
 # 3.5 Carreguem les dades dels artistes
+# db.artista.drop() # eliminem la colecció per si ja hi ha dades
 if db.artista.count_documents({})==0: # si no hi ha dades a la colecció artista, les carreguem
     print('\n5. Carregant dades dels artistes...')
     artista_dict=artists[['Nom_artistic','nom','cognoms','data_naix','pais']].to_dict('records') # utilizem format records perque cada fila sigue dict
